808b08025214442d1c30bae8fb96992375f56347
Brian Vu~ <berriiacn@gmail.com>
2025-08-18 11:45:21 +1000

    Added 3Sum

---
+++ Two_Pointers/3Sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        
+        for i, val in enumerate(nums):
+            if i > 0 and nums[i - 1] == val:
+                continue
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+                tripletSum = val + nums[l] + nums[r]
+                if tripletSum < 0:
+                    l += 1
+                elif tripletSum > 0:
+                    r -= 1
+                else:
+                    result.append([val, nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+
+        return result
+    
+from typing import List
