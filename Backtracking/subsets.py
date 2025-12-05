88d8f9673b0373bd1fe4d4b4de69705a9b16a330
Brian Vu~ <berriiacn@gmail.com>
2025-12-05 17:57:07 +1100

    Simplified subsets

--- Backtracking/subsets.py
+++ Backtracking/subsets.py
@@ -2,16 +2,16 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         subsets, curSet = [], []
 
-        def helper(i, nums, curSet, subsets):
+        def helper(i):
             if i >= len(nums):
                 subsets.append(curSet.copy())
                 return
 
             curSet.append(nums[i])
-            helper(i + 1, nums, curSet, subsets)
+            helper(i + 1)
             curSet.pop()
 
-            helper(i + 1, nums, curSet, subsets)
+            helper(i + 1)
         
-        helper(0, nums, curSet, subsets)
+        helper(0)
         return subsets
